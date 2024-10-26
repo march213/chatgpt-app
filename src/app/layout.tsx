2ca3ecb4f5903f0951e5876389bdfc1d024e836a
march213 <jane.molodetskaya@gmail.com>
2024-10-26 18:28:50 -0400

    adding layout and about route

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Link from "next/link";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -25,7 +26,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>{children}</body>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <header className="text-white font-bold bg-blue-900 text-2xl px-6 py-4 mb-3 rounded-b-lg shadow-blue-950 shadow-lg flex">
+          <div className="flex flex-grow gap-8">
+            <Link href="/">GPT Chat</Link>
+            <Link href="/about" className="font-light">
+              About
+            </Link>
+          </div>
+          <div></div>
+        </header>
+        <div className="flex flex-col md:flex-row">
+          <div className="flex-grow">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
